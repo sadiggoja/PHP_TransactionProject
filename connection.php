7a56d41f09b3dc8e663af36c84701f8f3c091309
Sadig <56036531+sadiggoja@users.noreply.github.com>
2021-04-12 15:41:52 +0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 
 $host='mysql-goja.alwaysdata.net';
 $user='goja';
-$passwd='31292467441898';
+$passwd='******';
 $bd='goja_a';
 $mysql = new mysqli($host,$user,$passwd,$bd) or die("error");;
 $mysql -> connect_errno;
